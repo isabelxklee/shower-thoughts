99bd16950f4b34300a2304d36617db5cad7b7cca
isabelxklee <hello@isabelklee.co>
2020-11-13 15:31:08 -0500

    added form validation

--- src/components/AddThoughtForm.jsx
+++ src/components/AddThoughtForm.jsx
@@ -2,12 +2,30 @@ import React from 'react'
 import {useFormik} from 'formik'
 
 const AddThoughtForm = () => {
+  const validate = (values) => {
+    const errors = {}
+
+    if (!values.emoji) {
+      errors.emoji = 'Required'
+    } else if (values.emoji.length < 1) {
+      errors.emoji = 'Please pick at least one emoji!'
+    }
+
+    if (!values.thought) {
+      errors.thought = 'Required'
+    } else if (values.thought.length < 30) {
+      errors.thought = 'A thought must be at least 30 characters long.'
+    }
+
+    return errors
+  }
 
   const formik = useFormik({
     initialValues: {
       emoji: '',
-      thought: ''
+      thought: '',
     },
+    validate,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2))
     },
@@ -26,6 +44,7 @@ const AddThoughtForm = () => {
         onChange={formik.handleChange}
         value={formik.values.emoji}
       />
+      {formik.errors.emoji ? <div>{formik.errors.emoji}</div> : null}
       <br />
 
       <label htmlFor="thought">What&apos;s your shower thought?</label>
@@ -38,6 +57,7 @@ const AddThoughtForm = () => {
         onChange={formik.handleChange}
         value={formik.values.thought}
       />
+      {formik.errors.thought ? <div>{formik.errors.thought}</div> : null}
       <br />
 
       <button type="submit">Create thought</button>
