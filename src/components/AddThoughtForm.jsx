51d342a9bc6913b1b7490aee107e70351e57822f
isabelxklee <hello@isabelklee.co>
2020-11-16 10:24:02 -0500

    reformatted add thought form and added yup validation

--- src/components/AddThoughtForm.jsx
+++ src/components/AddThoughtForm.jsx
@@ -1,66 +1,56 @@
 import React from 'react'
-// import {useFormik} from 'formik'
-2 import { Formik, Form, Field } from 'formik'
-3 import * as Yup from 'yup'
+import * as Yup from 'yup'
+import {Formik, Form, useField} from 'formik'
+
+const TextInput = ({label, ...props}) => {
+  const [field, meta] = useField(props)
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.error ? <div className="error">{meta.error}</div> : null}
+    </>
+  )
+}
+
+const formSchema = Yup.object().shape({
+  emoji: Yup.string()
+    .min(1, 'Please pick at least one emoji!')
+    .max(5, "Sorry, that's too many emojis!")
+    .required('Required'),
+  thought: Yup.string()
+    .min(30, 'Thoughts should be at least 30 characters.')
+    .max(250, "Sorry, that's too long for a thought.")
+    .required('Required'),
+})
 
 const AddThoughtForm = () => {
-  const validate = (values) => {
-    const errors = {}
-
-    if (values.emoji.length < 1) {
-      errors.emoji = 'Please pick at least one emoji!'
-    }
-
-    if (values.thought.length < 30) {
-      errors.thought = 'A thought must be at least 30 characters long.'
-    }
-
-    return errors
-  }
-
-  const formik = useFormik({
-    initialValues: {
-      emoji: '',
-      thought: '',
-    },
-    validate,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2))
-    },
-  })
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <h3>Add shower thought</h3>
+    <div>
+      <Formik
+        initialValues={{
+          emoji: '',
+          thought: '',
+        }}
+        validationSchema={formSchema}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        <Form>
+          <h3>Add shower thought</h3>
+          <TextInput label="Pick an emoji" name="emoji" type="text" autocomplete="off" />
 
-      <label htmlFor="emoji">Pick an emoji</label>
-      <br />
-      {formik.errors.emoji ? <div>{formik.errors.emoji}</div> : null}
-      <input
-        id="emoji"
-        name="emoji"
-        type="emoji"
-        autoComplete="off"
-        onChange={formik.handleChange}
-        value={formik.values.emoji}
-      />
-      <br />
-
-      <label htmlFor="thought">What&apos;s your shower thought?</label>
-      <br />
-      {formik.errors.thought ? <div>{formik.errors.thought}</div> : null}
-      <input
-        id="thought"
-        name="thought"
-        type="thought"
-        autoComplete="off"
-        onChange={formik.handleChange}
-        value={formik.values.thought}
-      />
-      <br />
-
-      <button type="submit">Create thought</button>
-    </form>
+          <TextInput
+            label="What's your shower thought?"
+            name="thought"
+            type="text"
+            autocomplete="off"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
   )
 }
 
